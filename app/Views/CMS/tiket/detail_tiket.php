6c737b007cf24e5cd8b95854ac69e575448b598b
gitroihan <roihanpklll@gmail.com>
2024-05-20 21:54:24 +0700

    detail tiket

--- app/Views/CMS/tiket/detail_tiket.php
+++ app/Views/CMS/tiket/detail_tiket.php
@@ -7,31 +7,35 @@ Tiket
 
 <div class="container-fluid">
     <?php foreach ($tiket as $tiket) : ?>
-        <table class="table table-borderless table-sm">
-            <tr>
-                <th style="width: 15%;">Nama</th>
-                <td style="width: 40%;">: <?= $tiket['nama_kontak'] ?></td>
-                <th style="width: 15%;">Prioritas</th>
-                <td>: <?= $tiket['prioritas'] ?></td>
-            </tr>
-            <tr>
-                <th>Email</th>
-                <td>: <?= $tiket['email'] ?></td>
-                <th>Subjek</th>
-                <td>: <?= $tiket['subjek'] ?></td>
-            </tr>
-            <tr>-
-                <th>Modul</th>
-                <td>: <?= $tiket['modul'] ?></td>
-            </tr>
-            <tr>
-                <th>Klasifikasi</th>
-                <td>: <?= $tiket['klasifikasi'] ?></td>
-            </tr>
-        </table>
-        <th>Deskripsi :</th>
-        <textarea id="editor" name="editor" class="form-control" readonly cols="30" rows="10"><?= $tiket['deskripsi'] ?></textarea>
+        <div class="card px-4 py-3 border-0 shadow">
+            <div class="p-2 pt-0 row">
+                <label for="nama" class="col-sm-1 col-form-label">Judul</label>
+                <div class="col-sm-11">
+                    <p class="form-control-plaintext">: <?= $tiket['nama_kontak'] ?></p>
+                </div>
+            </div>
+            <div class="p-2 pt-0 row">
+                <label for="email" class="col-sm-1 col-form-label">Email</label>
+                <div class="col-sm-11">
+                    <p class="form-control-plaintext">: <?= $tiket['email'] ?></p>
+                </div>
+            </div>
+            <div class="p-2 pt-0 row">
+                <label for="subjek" class="col-sm-1 col-form-label">Subjek</label>
+                <div class="col-sm-11">
+                    <p class="form-control-plaintext">: <?= $tiket['subjek'] ?></p>
+                </div>
+            </div>
+            <div class="p-2 pt-0 row">
+                <label for="deskripsi" class="col-sm-1 col-form-label">Deskripsi</label>
+                <div class="col-sm-11">
+                    <p class="form-control-plaintext">:  </p>
+                </div>
+                <textarea id="editor" name="editor" class="form-control" readonly cols="30" rows="10" style="background-color: white; height: 350px; overflow-y: auto;"><?= $tiket['deskripsi'] ?></textarea>
+            </div>
+        </div>
+        <button type="submit" class="btn text-light mt-3" style="background-color: #03C988;">ubah status</button>
     <?php endforeach; ?>
-    <button type="submit" class="btn text-light mt-3" style="background-color: #03C988;">ubah status</button>
 </div>
+
 <?php $this->endSection() ?>
--- app/Views/CMS/tiket/tiket.php
+++ app/Views/CMS/tiket/tiket.php
@@ -5,12 +5,12 @@ Tiket
 <?php $this->endSection() ?>
 <?php $this->section('content') ?>
 
-    <div class="container-fluid">
-        <!-- Page Heading -->
-        <div class="card px-4 py-3 border-0 shadow" >
-            <div class="d-sm-flex align-items-center justify-content-between mb-3">
-                <h1 class="h3 mr-auto mb-0 text-gray-800">Tiket</h1>
-            </div>
+<div class="container-fluid">
+    <!-- Page Heading -->
+    <div class="card px-4 py-3 border-0 shadow">
+        <div class="d-sm-flex align-items-center justify-content-between mb-3">
+            <h1 class="h3 mr-auto mb-0 text-gray-800">Tiket</h1>
+        </div>
         <table id="table_tiket" class="table table-hover table-md" style="text-align: center; ">
             <thead class="thead-dark">
                 <td style="width: 20%;">nama</td>
@@ -20,6 +20,6 @@ Tiket
             <tbody>
             </tbody>
         </table>
-        </div>
     </div>
+</div>
 <?php $this->endSection() ?>
