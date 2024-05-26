00bdffe07991e249bba5735659be9b18cf8e0bf5
gitroihan <roihanpklll@gmail.com>
2024-05-26 08:09:36 +0700

    detail tiket

--- app/Views/CMS/tiket/detail_tiket.php
+++ app/Views/CMS/tiket/detail_tiket.php
@@ -29,12 +29,14 @@ Tiket
             <div class="p-2 pt-0 row">
                 <label for="deskripsi" class="col-sm-1 col-form-label">Deskripsi</label>
                 <div class="col-sm-11">
-                    <p class="form-control-plaintext">:  </p>
+                    <p class="form-control-plaintext">: </p>
                 </div>
                 <textarea id="editor" name="editor" class="form-control" readonly cols="30" rows="10" style="background-color: white; height: 350px; overflow-y: auto;"><?= $tiket['deskripsi'] ?></textarea>
             </div>
         </div>
-        <button type="submit" class="btn text-light mt-3" style="background-color: #03C988;">ubah status</button>
+        <div class="mb-3 p-1 text-right">
+            <button type="submit" class="btn text-light mt-3" style="background-color: #03C988;">ubah status</button>
+        </div>
     <?php endforeach; ?>
 </div>
 
